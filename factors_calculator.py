9d369457661a312d740baf7097077647b8438131
KeysiYTS <62850338+KeysiYTS@users.noreply.github.com>
2022-07-15 22:32:45 -0300

    main class adjustments and feature addition
    Changes:
    - Creating a copy of the dataframe to avoid modifications in the original Dataframe;
    - Possibility to choose between algorithms;
    - Possibility to choose between imputer strategies (algorithms);
    - Removing the 'target_col' from the copy to avoid data leak;
    - Sort method to order the dataframe;
    - Setting RandomForestRegressor and KNNImputer as default strategies;
    
    Additions:
    - Random Forest  Regressor algorithm;
    - KNN Imputer for numerical columns;
    - Possibility to choose ammount of rows to be returned from get_coefficient method;
    - plot_importance method to visualize a barplot of the rows from the get_coefficient method.

--- factors_calculator.py
+++ factors_calculator.py
@@ -1,126 +1,163 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
-import pandas as pd
+
+from sklearn.impute import SimpleImputer
+from sklearn.impute import KNNImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.compose import make_column_transformer
+
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+
 
 class FactorsCalculator:
-    def __init__(
-        self, data, num_cols, cat_cols, target_col, random_st=42, 
-        factorize_target=True
-    ):
-        self.num_cols, self.cat_cols = num_cols, cat_cols
-        self.target_col = target_col
-
-        if factorize_target:
-            target, idx_to_label = data[target_col].factorize()
-            data['target'] = target
-            self.idx_to_label = idx_to_label
-        else:
-            data['target'] = data[target_col]
-            self.idx_to_label = [target_col]
-
-
-
-        self.train, self.test = train_test_split(
-            data, random_state=random_st
-        )
-        prep = self.get_pipeline_preprocessor()
-        self.clf = LogisticRegression()
-        self.model = self.get_model_pipeline(prep, self.clf)
-    
-    def fit_report(self):
-        self.model.fit(self.train, self.train[self.target_col])
-        print('Score:', self.model.score(self.test, self.test.target))
-        y_pred = self.model.predict(self.test)
-        print(classification_report(self.test.target, y_pred))
-
-    def _proc_output_names(self):
-        model = self.model
-
-        ct = model.steps[0][1]
-        ct_cat = ct.transformers_[0][1]
-        ct_cat_oh = ct_cat.steps[1][1]
-        cat_out_names_ = list(ct_cat_oh.get_feature_names_out())
-
-        for i, cat in enumerate(self.cat_cols):
-            prefix = f'x{i}'
-            for j, name in enumerate(cat_out_names_):
-                if prefix in name[:len(prefix)]:
-                    cat_out_names_[j] = name.replace(prefix, cat)
+  def __init__(self, data, num_cols, cat_cols,
+               target_col, algorithm='forest',
+               imputer_strategy='knn',
+               random_st=42, log_target=False):
+      df = data.copy()
+      self.num_cols, self.cat_cols = num_cols, cat_cols
+      self.target_col = target_col
+      self.imputer_strategy = imputer_strategy
+      self.algorithm = algorithm
+      
+      if log_target:
+          df['target'] = data[target_col].apply(np.log1p)
+      else:
+          df['target'] = data[target_col]
         
-        self.cat_out_names_ = cat_out_names_
+      df.drop(columns='answer', inplace=True)
+      
+      if algorithm =='forest':
+        self.clf = RandomForestRegressor(random_state=random_st)
+        print('running random forest regressor\n')
+      elif algorithm =='linear':
+        self.clf = LinearRegression()
+        print('running linear regression \n')
 
-        ct_num = ct.transformers_[1][1]
-        ct_num_si = ct_num.steps[0][1]
-        self.num_out_names_ = list(ct_num_si.feature_names_in_)
+      self.train, self.test = train_test_split(
+                              df, random_state=random_st)
+      
+      prep = self.get_pipeline_preprocessor()
+      
+      self.model = self.get_model_pipeline(prep, self.clf)
 
-        return self.cat_out_names_ + self.num_out_names_
+  def get_pipeline_preprocessor(self):
+    if self.imputer_strategy=='knn':
+      self.num_out_transformer = KNNImputer(n_neighbors=3)
+      print('running KNNImputer \n')
+    elif self.imputer_strategy=='simple':
+      self.num_out_transformer = SimpleImputer(strategy='median')
+      print('running SimpleImputer \n')
+      
+    cat_pipe = Pipeline(
+        [
+            ('imputer', SimpleImputer(strategy='constant')),
+            ('one_hot', OneHotEncoder(sparse=False,handle_unknown='ignore')),
+                                      #min_frequency=0.05)
+            #('scaler', StandardScaler())
+        ]
+    )
+    num_pipe = Pipeline(
+        [
+            ('imputer', self.num_out_transformer),
+            ('scaler', StandardScaler()) # Needed for coeff outputting Z-Score
+        ]
+    )
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('cat', cat_pipe, self.cat_cols),
+            ('num', num_pipe, self.num_cols)
+        ]
+    )  
+    return preprocessor
 
-    def get_coefficients(self):
+  def get_model_pipeline(self, preprocessor, clf):
+      model = Pipeline(
+          steps=[
+              ('preprocessor', preprocessor),
+              ('estimator', clf)
+          ]
+      )
+      return model
+
+  def fit_report(self):
+      self.model.fit(self.train, self.train['target'])
+      print('R2 Score:', self.model.score(self.test, self.test.target))
+      y_pred = self.model.predict(self.test)
+      rmse = mean_squared_error(self.test.target, y_pred, squared=False)
+      print('RMSE:', rmse)
+
+  def _proc_output_names(self):
+      model = self.model
+      ct = model.steps[0][1]
+      ct_cat = ct.transformers_[0][1]
+      ct_cat_oh = ct_cat.steps[1][1]
+      cat_out_names_ = list(ct_cat_oh.get_feature_names_out())
+      for i, cat in enumerate(self.cat_cols):
+          prefix = f'x{i}'
+          for j, name in enumerate(cat_out_names_):
+              if prefix in name[:len(prefix)]:
+                  cat_out_names_[j] = name.replace(prefix, cat)
+      
+      self.cat_out_names_ = cat_out_names_
+      ct_num = ct.transformers_[1][1]
+      ct_num_si = ct_num.steps[0][1]
+      self.num_out_names_ = list(ct_num_si.feature_names_in_)
+      return self.cat_out_names_ + self.num_out_names_
+
+  def get_coefficients(self, asc=False, return_rows=5):
+      if self.algorithm =='linear':
+        print('Getting linear coefficients...')
         output_names = self._proc_output_names()
-        coef = pd.DataFrame(
-            self.clf.coef_,
-            columns=output_names,
-            #index=self.idx_to_label
-        ).T
 
-        sorted_idx = coef.apply(
-            lambda x: abs(sum(x)), axis=1
-        ).sort_values(ascending=False).index
+        df_map = {'features': output_names,
+                  'importance':self.clf.coef_}
+        coef = pd.DataFrame(df_map)
+        coef.sort_values(by='importance', 
+                         ascending=asc, 
+                         inplace=True)
 
-        coef = coef.loc[sorted_idx]
-
-        return coef
-    
-    def get_shap(self):
-        pass
-    
-    def report_top_factors(self):
-        pass
-
-    def get_pipeline_preprocessor(self):
-        self.num_out_transformer = SimpleImputer(strategy='median')
-
-        cat_pipe = Pipeline(
-            [
-                ('imputer', SimpleImputer(strategy='constant')),
-                ('one_hot', OneHotEncoder(
-                    sparse=False,
-                    handle_unknown='infrequent_if_exist',
-                    min_frequency=0.05 # ignore minority categories
-                )),
-                ('scaler', StandardScaler())
-             
-            ]
-        )
-
-        num_pipe = Pipeline(
-            [
-                ('imputer', self.num_out_transformer),
-                ('scaler', StandardScaler()) # Needed for coeff outputting Z-Score
-            ]
-        )
-
-        preprocessor = ColumnTransformer(
-            transformers=[
-                ('cat', cat_pipe, self.cat_cols),
-                ('num', num_pipe, self.num_cols)
-            ]
-        )
-    
-        return preprocessor
-
-    def get_model_pipeline(self, preprocessor, clf):
-        model = Pipeline(
-            steps=[
-                ('preprocessor', preprocessor),
-                ('estimator', clf)
-            ]
-        )
+        coef = coef[coef.features != "target"]
+        rows = coef.shape
+        if return_rows > rows[0]:
+          print(f'printing head \n')
+          coef = coef.head()
+        else:
+          print(f'printing {return_rows} rows\n')
+          coef = coef.head(return_rows)
+ 
+      elif self.algorithm =='forest':
+        print('Getting forest coefficients...')
+        output_names = self._proc_output_names()
+        df_map = {'features': output_names,
+                  'importance':self.clf.feature_importances_}
+        coef = pd.DataFrame(df_map)
+        coef.sort_values(by='importance', 
+                         ascending=asc, 
+                         inplace=True)
         
-        return model
+        coef = coef[coef.features != "target"]
+        rows = coef.shape[0]
+        if return_rows > rows:
+          print(f'printing head \n')
+          coef = coef.head()
+        else:
+          print(f'printing {return_rows} rows\n')
+          coef = coef.head(return_rows)
+
+      return coef
+
+  def plot_importance(self, coef_df, graph_title="Model feature importance"):
+    sns.barplot(data=coeffs,x='importance',
+                y='features').set(title=graph_title)
+    return 
