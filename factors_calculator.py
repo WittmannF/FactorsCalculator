429ab1aabbc23386c894bd80c72de990ab1d4183
Fernando Marcos Wittmann <fernando.wittmann@gmail.com>
2022-08-22 11:33:51 -0300

    add MedianKNNRegressor

--- factors_calculator.py
+++ factors_calculator.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import classification_report
 from sklearn.metrics import mean_squared_error
 from sklearn.pipeline import Pipeline
-from catboost import CatBoostRegressor
+from catboost import CatBoostRegressor, CatBoostClassifier
 import matplotlib.pyplot as plt
 
 from sklearn.compose import ColumnTransformer
@@ -19,15 +19,58 @@ import shap
 import pandas as pd
 import numpy as np
 
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors._base import _get_weights
+from sklearn.utils.validation import check_array
+
+class MedianKNNRegressor(KNeighborsRegressor):
+    def predict(self, X, return_match_index=False):
+        X = check_array(X, accept_sparse='csr')
+
+        neigh_dist, neigh_ind = self.kneighbors(X)
+
+        weights = _get_weights(neigh_dist, self.weights)
+
+        _y = self._y
+        if _y.ndim == 1:
+            _y = _y.reshape((-1, 1))
+
+        ######## Begin modification
+        if weights is None:
+            y_pred = np.median(_y[neigh_ind], axis=1)
+        else:
+            # y_pred = weighted_median(_y[neigh_ind], weights, axis=1)
+            raise NotImplementedError("weighted median")
+        ######### End modification
+
+        if self._y.ndim == 1:
+            y_pred = y_pred.ravel()
+
+        if return_match_index:
+            d = _y[neigh_ind]
+            middle_idx = self.n_neighbors//2 #assumes n_neighbors is odd
+            median_idx = np.argsort(d)[:, middle_idx]
+            nearest_index = []
+            for ni, mi in zip(neigh_ind, median_idx):
+                nearest_index.append(ni[mi])
+
+            nearest_matched_index = np.array(nearest_index)
+
+            return y_pred, nearest_matched_index
+        else:
+            return y_pred
+
 
 
 class FactorsCalculatorRegressor:
     def __init__(
         self,
-        data,
         num_cols, 
         cat_cols, 
-        target_col, 
+        target_col,
+        data=None,
+        train=None,
+        test=None,
         random_st=42, 
         log_target=False,
         estimator='catboost',
@@ -37,25 +80,34 @@ class FactorsCalculatorRegressor:
         self.estimator = estimator
         self.target_col = target_col
         self.imputer_strategy = imputer_strategy
-        self.data = data.copy()
+        if data is not None:
+            self.data = data.copy()
 
-        if log_target:
-            self.data['target'] = self.data[target_col].apply(np.log1p)
+            if log_target:
+                self.data['target'] = self.data[target_col].apply(np.log1p)
+            else:
+                self.data['target'] = self.data[target_col]
+
+            self.data.drop(columns=target_col, inplace=True)
+
+            self.train, self.test = train_test_split(
+                self.data, random_state=random_st
+            )
         else:
-            self.data['target'] = self.data[target_col]
-
-        self.data.drop(columns=target_col, inplace=True)
-
-        self.train, self.test = train_test_split(
-            self.data, random_state=random_st
-        )
+            self.train = train.copy()
+            self.train['target'] = self.train[target_col]
+            self.train.drop(columns=target_col, inplace=True)
+            self.test = test.copy()
+            self.test['target'] = self.test[target_col]
+            self.test.drop(columns=target_col, inplace=True)
+            self.data = pd.concat([train, test])
 
         prep = self.get_pipeline_preprocessor()
 
         if estimator=='random_forest':
             self.reg = RandomForestRegressor(random_state=random_st)
         elif estimator=='catboost':
-            self.reg = CatBoostRegressor()
+            self.reg = CatBoostRegressor(verbose=0)
         else:
             self.reg = LinearRegression()
         
@@ -66,8 +118,8 @@ class FactorsCalculatorRegressor:
     def fit_report(self):
         self.model.fit(self.train, self.train.target)
         print('R2 Score:', self.model.score(self.test, self.test.target))
-        y_pred = self.model.predict(self.test)
-        rmse = mean_squared_error(self.test.target, y_pred, squared=False)
+        self.y_pred = self.model.predict(self.test)
+        rmse = mean_squared_error(self.test.target, self.y_pred, squared=False)
         print('RMSE:', rmse)
 
     def _proc_output_names(self):
@@ -206,7 +258,7 @@ class FactorsCalculatorRegressor:
                     handle_unknown='infrequent_if_exist',
                     min_frequency=0.05 # ignore minority categories
                 )),
-                ('scaler', StandardScaler())
+                #('scaler', StandardScaler())
              
             ]
         )
@@ -214,7 +266,7 @@ class FactorsCalculatorRegressor:
         num_pipe = Pipeline(
             [
                 ('imputer', self.num_out_transformer),
-                ('scaler', StandardScaler()) # Needed for coeff outputting Z-Score
+                #('scaler', StandardScaler()) # Needed for coeff outputting Z-Score
             ]
         )
 
@@ -237,36 +289,61 @@ class FactorsCalculatorRegressor:
         
         return model
 
-class FactorsCalculator:
+class FactorsCalculatorClassifier(object):
     def __init__(
-        self, data, num_cols, cat_cols, target_col, random_st=42, 
-        factorize_target=True
-    ):
+        self,
+        num_cols, 
+        cat_cols, 
+        target_col,
+        data=None,
+        train=None,
+        test=None,
+        random_st=42, 
+        log_target=False,
+        estimator='catboost',
+        imputer_strategy='simple',
+):
         self.num_cols, self.cat_cols = num_cols, cat_cols
+        self.estimator = estimator
         self.target_col = target_col
+        self.imputer_strategy = imputer_strategy
+        if data is not None:
+            self.data = data.copy()
 
-        if factorize_target:
-            target, idx_to_label = data[target_col].factorize()
-            data['target'] = target
-            self.idx_to_label = idx_to_label
+            if log_target:
+                self.data['target'] = self.data[target_col].apply(np.log1p)
+            else:
+                self.data['target'] = self.data[target_col]
+
+            self.data.drop(columns=target_col, inplace=True)
+
+            self.train, self.test = train_test_split(
+                self.data, random_state=random_st
+            )
         else:
-            data['target'] = data[target_col]
-            self.idx_to_label = [target_col]
+            self.train = train.copy()
+            self.train['target'] = self.train[target_col]
+            self.train.drop(columns=target_col, inplace=True)
+            self.test = test.copy()
+            self.test['target'] = self.test[target_col]
+            self.test.drop(columns=target_col, inplace=True)
+            self.data = pd.concat([train, test])
 
-
-
-        self.train, self.test = train_test_split(
-            data, random_state=random_st
-        )
         prep = self.get_pipeline_preprocessor()
-        self.clf = LogisticRegression()
-        self.model = self.get_model_pipeline(prep, self.clf)
+
+        if estimator=='catboost':
+            self.estimator = CatBoostClassifier(verbose=0)
+        
+        print('Using estimator', self.estimator)
+        
+        self.model = self.get_model_pipeline(prep, self.estimator)
     
     def fit_report(self):
-        self.model.fit(self.train, self.train[self.target_col])
+        self.model.fit(self.train, self.train.target)
         print('Score:', self.model.score(self.test, self.test.target))
-        y_pred = self.model.predict(self.test)
-        print(classification_report(self.test.target, y_pred))
+        self.y_pred = self.model.predict(self.test)
+        print('Classification report')
+        print(classification_report(self.test.target, self.y_pred))
 
     def _proc_output_names(self):
         model = self.model
@@ -289,31 +366,109 @@ class FactorsCalculator:
         self.num_out_names_ = list(ct_num_si.feature_names_in_)
 
         return self.cat_out_names_ + self.num_out_names_
+    
+    def test_binary_column(self, binary_column):
+        print('Significance test on', binary_column)
+        filter_feature = self.data[binary_column]==1
+        with_feature = self.data.loc[filter_feature, 'target'].values
+        without_feature = self.data.loc[~filter_feature, 'target'].values
+        sns.histplot(
+            x="target", 
+            hue=binary_column, 
+            data=self.data,
+            multiple="dodge",
+            binwidth=1
+            #element="step"
+        ).set_title(
+            'Distributions of binary feature'
+        )
+        plt.show()
+        sns.boxplot(
+            x=binary_column,
+            y='target',
+            data=self.data
+        ).set_title(
+            'Boxplot of binary feature'
+        )
+        plt.show()
+        U1, p = mannwhitneyu(with_feature, without_feature, alternative='two-sided')
+        print('Two-sided p-value for Mann–Whitney U test is', p)
+        return p
 
     def get_coefficients(self):
+        if self.estimator=='linear':
+            output_names = self._proc_output_names()
+            #coef = pd.DataFrame(
+            #    self.estimator.coef_,
+            #    index=output_names
+            #)
+
+            coef = pd.DataFrame(
+                {
+                    'features': output_names,
+                    'importance':self.clf.coef_
+                }
+            )
+
+            sorted_idx = coef.apply(
+                lambda x: abs(sum(x)), axis=1
+            ).sort_values(ascending=False).index
+
+            coef = coef.loc[sorted_idx]
+
+            return coef
+        elif self.estimator=='random_forest' or self.estimator=='catboost':
+            output_names = self._proc_output_names()
+            feat_imp = pd.DataFrame(
+                {
+                    'features': output_names,
+                    'importance': self.estimator.feature_importances_
+                }   
+            )
+
+            feat_imp = feat_imp.sort_values(
+                by='importance',
+                ascending=False
+            )
+
+            return feat_imp
+
+
+    def get_feature_importances(self, plot=True):
         output_names = self._proc_output_names()
-        coef = pd.DataFrame(
-            self.clf.coef_,
-            columns=output_names,
-            #index=self.idx_to_label
-        ).T
+        feat_imp = pd.Series(
+            self.estimator.feature_importances_,
+            index=output_names
+        )
 
-        sorted_idx = coef.apply(
-            lambda x: abs(sum(x)), axis=1
-        ).sort_values(ascending=False).index
+        feat_imp = feat_imp.sort_values(ascending=False)
 
-        coef = coef.loc[sorted_idx]
-
-        return coef
+        return feat_imp
     
-    def get_shap(self):
-        pass
+    def get_shap(self, return_shap_values=False):
+        explainer = shap.TreeExplainer(self.model['estimator'])
+        observations = self.model['preprocessor'].transform(self.test)
+        feature_names = self._proc_output_names()
+        observations = pd.DataFrame(observations, columns=feature_names)
+        shap_values = explainer(observations)
+        shap.summary_plot(shap_values, observations)
+        if return_shap_values:
+            return shap_values
     
-    def report_top_factors(self):
-        pass
+    def plot_importance(
+        self,
+        feat_imp,
+        graph_title="Model feature importance"
+    ):
+        sns.barplot(feat_imp).set(title=graph_title)
+        return
 
     def get_pipeline_preprocessor(self):
-        self.num_out_transformer = SimpleImputer(strategy='median')
+        if self.imputer_strategy=='knn':
+            self.num_out_transformer = KNNImputer(n_neighbors=3)
+        elif self.imputer_strategy=='simple':
+            self.num_out_transformer = SimpleImputer(strategy='median')
+        print('Imputer strategy:', self.num_out_transformer)
 
         cat_pipe = Pipeline(
             [
@@ -321,9 +476,9 @@ class FactorsCalculator:
                 ('one_hot', OneHotEncoder(
                     sparse=False,
                     handle_unknown='infrequent_if_exist',
-                    min_frequency=0.05 # ignore minority categories
+                    #min_frequency=0.05 # ignore minority categories
                 )),
-                ('scaler', StandardScaler())
+                #('scaler', StandardScaler())
              
             ]
         )
@@ -331,7 +486,7 @@ class FactorsCalculator:
         num_pipe = Pipeline(
             [
                 ('imputer', self.num_out_transformer),
-                ('scaler', StandardScaler()) # Needed for coeff outputting Z-Score
+                #('scaler', StandardScaler()) # Needed for coeff outputting Z-Score
             ]
         )
 
@@ -344,11 +499,11 @@ class FactorsCalculator:
     
         return preprocessor
 
-    def get_model_pipeline(self, preprocessor, clf):
+    def get_model_pipeline(self, preprocessor, reg):
         model = Pipeline(
             steps=[
                 ('preprocessor', preprocessor),
-                ('estimator', clf)
+                ('estimator', reg)
             ]
         )
         
